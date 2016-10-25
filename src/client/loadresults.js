7d73576a35097ba8100ad7aa7c4e7524504f2ada
Alonso Robles <alonso.robles@outlook.com>
2016-10-25 12:14:05 -0500

    Removed console logging.

--- src/client/loadresults.js
+++ src/client/loadresults.js
@@ -1,7 +1,6 @@
 $(function() {
     $.getJSON("https://azurebootcamp2016.azurewebsites.net/api/categoryresults?category=AzureService", function(data) {
         var items = _.chain(data).sortBy(function(item){return item.count}).map(function(item) { return "<li>" + item.candidate + " (" + item.count + " votes)</li>"}).value();
-        console.log(items);
         $("div#AzureServiceResults").html(
             $("<ul />", {
                 html: items.join("")
@@ -10,7 +9,6 @@ $(function() {
     });
     $.getJSON("https://azurebootcamp2016.azurewebsites.net/api/categoryresults?category=Superhero", function(data) {
         var items = _.chain(data).sortBy(function(item){return item.count}).map(function(item) { return "<li>" + item.candidate + " (" + item.count + " votes)</li>"}).value();
-        console.log(items);
         $("div#SuperheroResults").html(
             $("<ul />", {
                 html: items.join("")
